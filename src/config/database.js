e88d826d4d7bb45db474c254b63a0437cf4f319b
Matheus Zenker <matheus.g.zenker@gmail.com>
2019-06-02 22:35:43 -0300

    Configuração banco Mongo, requires de arquivos corrigidos.

---
+++ src/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  uri:
+    'mongodb+srv://appmarketplace:1YtO31hwsFJBCvg5@appmarketplace-hfwgn.mongodb.net/test?retryWrites=true&w=majority'
+}
--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
@@ -14,7 +24,7 @@ class App {
   }
 
   routes () {
-    this.express.use(require('/.routes'))
+    this.express.use(require('./routes'))
   }
 }
 
